ed649329ac87fedca86db2a1623f26a4ff5fbaf9
angelozerr <angelo.zerr@gmail.com>
2018-05-16 12:24:56 +0200

    Show rgb color in square while debugging.

--- org.eclipse.jdt.codemining/src/org/eclipse/jdt/internal/ui/javaeditor/codemining/debug/InlinedDebugCodeMining.java
+++ org.eclipse.jdt.codemining/src/org/eclipse/jdt/internal/ui/javaeditor/codemining/debug/InlinedDebugCodeMining.java
@@ -11,10 +11,13 @@
 package org.eclipse.jdt.internal.ui.javaeditor.codemining.debug;
 
 import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.model.IVariable;
 import org.eclipse.debug.internal.ui.views.launch.DebugElementHelper;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.debug.core.IJavaStackFrame;
+import org.eclipse.jdt.debug.core.IJavaValue;
 import org.eclipse.jdt.debug.core.IJavaVariable;
+import org.eclipse.jdt.internal.ui.javaeditor.codemining.JavaMethodParameterCodeMiningProvider;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
@@ -25,8 +28,11 @@ import org.eclipse.jface.text.codemining.LineContentCodeMining;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.FontMetrics;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
 
 /**
  * Show start statement text in the end statement as mining.
@@ -35,6 +41,7 @@ import org.eclipse.swt.graphics.Point;
 public abstract class InlinedDebugCodeMining extends LineContentCodeMining {
 
 	private final IJavaStackFrame frame;
+	private RGB rgb;
 
 	protected InlinedDebugCodeMining(ASTNode node, IJavaStackFrame frame, ITextViewer viewer,
 			ICodeMiningProvider provider) {
@@ -56,6 +63,22 @@ public abstract class InlinedDebugCodeMining extends LineContentCodeMining {
 		try {
 			IJavaVariable variable = frame.findVariable(variableName);
 			if (variable != null) {
+				IJavaValue v = (IJavaValue) variable.getValue();
+				if ("org.eclipse.swt.graphics.RGB".equals(v.getJavaType().toString())) {
+					Integer red = null, blue = null, green = null;
+					for (IVariable field : v.getVariables()) {
+						if ("red".equals(field.getName().toString())) {
+							red = Integer.parseInt(field.getValue().getValueString());
+						} else if ("green".equals(field.getName().toString())) {
+							green = Integer.parseInt(field.getValue().getValueString());
+						} else if ("blue".equals(field.getName().toString())) {
+							blue = Integer.parseInt(field.getValue().getValueString());
+						}
+					}
+					if (red != null && green != null && blue != null) {
+						rgb = new RGB(red, green, blue);
+					}
+				}
 				String s = " " + DebugElementHelper.getLabel(variable);
 				super.setLabel(s);
 			} else {
@@ -74,7 +97,51 @@ public abstract class InlinedDebugCodeMining extends LineContentCodeMining {
 		Point space = gc.stringExtent(" ");
 		gc.fillRectangle(x + space.x, y, p.x, p.y);
 		gc.drawString(title, x, y, true);
+		if (rgb != null) {
+			int width = drawSquare(rgb, gc, textWidget, x + p.x, y);
+			p.x += width;
+		}
 		return p;
 	}
 
+	private int drawSquare(RGB rgb, GC gc, StyledText textWidget, int x, int y) {
+		FontMetrics fontMetrics = gc.getFontMetrics();
+		int size = getSquareSize(fontMetrics);
+		x += fontMetrics.getLeading();
+		y += fontMetrics.getDescent();
+
+		Rectangle rect = new Rectangle(x, y, size, size);
+
+		// Fill square
+		gc.setBackground(
+				((JavaMethodParameterCodeMiningProvider) getProvider()).getColor(rgb, textWidget.getDisplay()));
+		gc.fillRectangle(rect);
+
+		// Draw square box
+		gc.setForeground(textWidget.getForeground());
+		gc.drawRectangle(rect);
+		return getSquareWidth(gc.getFontMetrics());
+	}
+
+	/**
+	 * Returns the colorized square size.
+	 *
+	 * @param fontMetrics
+	 * @return the colorized square size.
+	 */
+	public static int getSquareSize(FontMetrics fontMetrics) {
+		return fontMetrics.getHeight() - 2 * fontMetrics.getDescent();
+	}
+
+	/**
+	 * Compute width of square
+	 *
+	 * @param styledText
+	 * @return the width of square
+	 */
+	private static int getSquareWidth(FontMetrics fontMetrics) {
+		// width = 2 spaces + size width of square
+		int width = 2 * fontMetrics.getAverageCharWidth() + getSquareSize(fontMetrics);
+		return width;
+	}
 }
